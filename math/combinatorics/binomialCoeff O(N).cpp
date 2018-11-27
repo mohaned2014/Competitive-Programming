b4de91aefc80a3aa5536ff29c7c26e5059c86221
Mohaned El-Haddad <30885720+mohaned2014@users.noreply.github.com>
2018-11-27 13:21:09 +0200

    Create binomialCoeff O(N).cpp
    binomialCoeff O(N).

---
+++ math/combinatorics/binomialCoeff O(N).cpp
@@ -0,0 +1,16 @@
+unsigned long long binomialCoeff(int n, int k)
+{
+    unsigned ll res = 1;
+
+    // Since C(n, k) = C(n, n-k)
+    if ( k > n - k )
+        k = n - k;
+    // Calculate value of [n * (n-1) *---* (n-k+1)] / [k * (k-1) *----* 1]
+    for (unsigned ll i = 0; i < k; ++i)
+    {
+        res *= (n - i);
+        res /= (i + 1);
+    }
+
+    return res;
+}
